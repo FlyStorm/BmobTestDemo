c8f01cf6959ad72d47c39edfe571e23c4d091c6c
FlyStorm <ggloading@gmail.com>
2017-10-22 01:08:15 +0800

    配置manifests,初始化BmobSDK

--- app/src/main/java/com/bmobtestdemo/fi/MainActivity.java
+++ app/src/main/java/com/bmobtestdemo/fi/MainActivity.java
@@ -1,13 +1,18 @@
 package com.bmobtestdemo.fi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cn.bmob.v3.Bmob;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //第一：默认初始化
+        Bmob.initialize(this, "771c17303ade44118e1bb1ee51dec111");
     }
 }
