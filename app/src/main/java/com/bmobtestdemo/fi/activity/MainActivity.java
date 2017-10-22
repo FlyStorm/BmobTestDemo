b9b19a21bfa76939cbe5ba03fa6b845ef5046fe7
FlyStorm <ggloading@gmail.com>
2017-10-22 10:12:30 +0800

    删除一行数据，修改一行数据，查询一行数据

--- app/src/main/java/com/bmobtestdemo/fi/activity/MainActivity.java
+++ app/src/main/java/com/bmobtestdemo/fi/activity/MainActivity.java
@@ -10,8 +10,11 @@ import com.bmobtestdemo.fi.R;
 import com.bmobtestdemo.fi.bean.Person;
 
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UpdateListener;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -51,7 +54,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_addData:
+            case R.id.btn_addData://添加一行数据
                 Person p1=new Person();
                 p1.setName("孙悟空");
                 p1.setAddress("花果山");
@@ -67,10 +70,51 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 });
                 break;
             case R.id.btn_delData:
+                final Person p3 = new Person();
+                p3.setObjectId("012a5cef4b");
+                p3.delete(new UpdateListener() {
+
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                            Toast.makeText(MainActivity.this,"删除成功:"+p3.getUpdatedAt(),Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"删除失败：" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
                 break;
             case R.id.btn_updateData:
+                //更新Person表里面id为6b6c11c537的数据，address内容更新为“北京朝阳”
+                final Person p2 = new Person();
+                p2.setAddress("天庭");
+                p2.update("012a5cef4b", new UpdateListener() {
+
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                            Toast.makeText(MainActivity.this,"更新成功："+p2.getUpdatedAt(),Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"更新失败：" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
                 break;
             case R.id.btn_queryData:
+                //查找Person表里面id为6b6c11c537的数据
+                BmobQuery<Person> bmobQuery = new BmobQuery<Person>();
+                bmobQuery.getObject("012a5cef4b", new QueryListener<Person>() {
+                @Override
+                public void done(Person object,BmobException e) {
+                    if(e==null){
+                        Toast.makeText(MainActivity.this,"查询成功",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this,"查询失败"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
                 break;
         }
     }
